12e7c21b32d1b7cf8818361e482d93f4a72aed69
Jacob Wysko <wyskoj@gmail.com>
2020-05-18 22:18:02 -0400

    Fix license

--- src/main/java/GTA5Node.java
+++ src/main/java/GTA5Node.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) %today.year Jacob Wysko
+ * Copyright (c) 2020 Jacob Wysko
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
